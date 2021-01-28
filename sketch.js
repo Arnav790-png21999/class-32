aa786da167253f5a13643487283902e5aa1de95c
Arnav790-png21999 <75166961+Arnav790-png21999@users.noreply.github.com>
2021-01-28 14:57:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var gameState=0
+var database;
+var playerCount
+var form,player,game;
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+   
+    
+}
+
+function draw(){
+    background("white");
+    
+    
+}
+
+
+
